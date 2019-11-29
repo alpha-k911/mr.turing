d85a31b417d6b73b30a262d9c2f27ce144a70033
alpha-k911 <34766587+alpha-k911@users.noreply.github.com>
2019-11-30 00:18:15 +0530

    initial

---
+++ cmake-build-debug/CMakeFiles/toc3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/toc3.dir/main.cpp.o"
+  "toc3"
+  "toc3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/toc3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/toc3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for toc3.
+# This may be replaced when dependencies are built.
